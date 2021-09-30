3e16d5ca195cb564f8980d7b4bb69633fb2b5644
danny-77 <91527434+danny-77@users.noreply.github.com>
2021-09-30 17:58:53 +1000

    new

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
-console.log('hello')
+// console.log('hello')
+//
+// let loginButton = document.querySelector('.login-button')
+// console.log(loginButton)
+// function openModal(){
+//  console.log("button clicked")
+// }
+// loginButton.addEventListener("click", openModal)
 
-let loginButton = document.querySelector('.login-button')
-console.log(loginButton)
-function openModal(){
- console.log("button clicked")
-}
-loginButton.addEventListener("click", openModal)
+
+document.getElementsByClassName("login").addEventListener("click", function(){
+  document.querySelector(".login-modal").style.display = "flex";
+})
